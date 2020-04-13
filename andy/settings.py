339aa6b9298a94f2f243845e932c512dde596d71
ohongs <samkahunga65@gmail.com>
2020-04-14 02:11:36 +0300

    ''

--- andy/settings.py
+++ andy/settings.py
@@ -129,9 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# CORS_ORIGIN_WHITELIST = [
-#     "http://127.0.0.1:3000"
-# ]
-# CORS_ORIGIN_ALLOW_ALL = True
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
