7acaf0c36325ca61f892e922911e6ae9bb0876d9
ohongs <samkahunga65@gmail.com>
2020-04-14 02:35:09 +0300

    l

--- andy/settings.py
+++ andy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=^^azy@oet4zp-&jr0_29g!4_p6=^@yo@#6#do(^bc!(ppmqi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'andyapi.herokuapp.com']
 
 
 # Application definition
